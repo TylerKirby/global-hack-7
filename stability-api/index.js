97e7984e9482b2d660c56cb0df26f16fd69f551a
Alex Simons <alex_simons@unigroup.com>
2018-10-13 01:13:35 -0500

    More able to do rest calls now :)

--- stability-api/index.js
+++ stability-api/index.js
@@ -1,5 +1,6 @@
-const {ApolloServer, gql, MockList,AuthenticationError} = require('apollo-server');
+const {ApolloServer, gql, MockList, AuthenticationError} = require('apollo-server');
 const {find, filter} = require('lodash');
+const {MoviesAPI} = require('./MoviesAPI');
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
@@ -80,6 +81,10 @@ const typeDefs = gql`
     books: [Book]
   }
   
+  type Movie {
+    title: String
+  }
+  
   type Person {
     name: String
     age: Int
@@ -89,6 +94,7 @@ const typeDefs = gql`
     author: Author
     people: [Person]
     readError: String
+    movie(id: Int): Movie
     authenticationError: String
     getAuthor(id: Int): Author
   }
@@ -96,10 +102,10 @@ const typeDefs = gql`
 `;
 
 const mocks = {
-  Query: () =>({
+  Query: () => ({
     people: () => new MockList([0, 12]),
   }),
-  String: () => `I am a mock: ${Math.ceil(100 *Math.random())}`,
+  String: () => `I am a mock: ${Math.ceil(100 * Math.random())}`,
 };
 
 const resolvers = {
@@ -107,19 +113,22 @@ const resolvers = {
     author(root, args, context, info) {
       return find(authors, {id: args.id});
     },
+    movie: async (_source, { id }, { dataSources }) => {
+      return dataSources.moviesAPI.getMovie(id);
+    },
     readError: (parent, args, context) => {
       fs.readFileSync('/does/not/exist');
     },
     authenticationError: (parent, args, context) => {
       throw new AuthenticationError('must authenticate');
     },
-    getAuthor(root, args, context, info){
+    getAuthor(root, args, context, info) {
       return find(authors, {id: args.id})
     }
   },
   Author: {
     books(root, args, context, info) {
-      return books.filter(book=> book.author.id === root.id)
+      return books.filter(book => book.author.id === root.id)
     },
   },
 };
@@ -127,7 +136,16 @@ const resolvers = {
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({typeDefs, resolvers, mocks, mockEntireSchema: false});
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    moviesAPI: new MoviesAPI(),
+  }),
+  mocks,
+  mockEntireSchema: false,
+
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
