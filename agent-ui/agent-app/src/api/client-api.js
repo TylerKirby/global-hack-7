883ffaeea3c88e1be8dd422dd2e20490b2762d2e
Jason_Hatton <Jason_Hatton@unigroup.com>
2018-10-13 17:36:48 -0500

    removed unused mutation reference

--- agent-ui/agent-app/src/api/client-api.js
+++ agent-ui/agent-app/src/api/client-api.js
@@ -2,16 +2,6 @@ import gql from 'graphql-tag';
 
 import { apollo } from 'api/apollo';
 
-const ADD_TODO = gql`
-  mutation AddTodo($type: String!) {
-    addTodo(type: $type) {
-      id
-      type
-    }
-  }
-`;
-
-
 export const addClientMutation = gql`
   mutation AddAnUnstable($anUnstable: TheUnstableInput!) {
     addAnUnstable(anUnstable: $anUnstable) {
