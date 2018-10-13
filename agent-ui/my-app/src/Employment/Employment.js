596c3d2180a79f7369b961641a6ce349d4076451
Alex Simons <alex_simons@unigroup.com>
2018-10-13 03:05:57 -0500

    More apollo client for stability ui

--- agent-ui/my-app/src/Employment/Employment.js
+++ agent-ui/my-app/src/Employment/Employment.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import { Container, Button } from 'semantic-ui-react';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
 
 class Employment extends Component {
+  componentDidMount(){
+    client.query({
+          query: gql`
+      query {
+        employmentOpportunitiesForId(id: 4) {
+          name
+          id
+          type
+          description
+          imageUrl
+        }
+      }
+    `
+        })
+        .then(result => console.log(result));
+
+  }
   render() {
     return (
       <div className="Employment">
